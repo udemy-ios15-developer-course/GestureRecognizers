b92e8e96c4479bd90a3d189529f76391a13a3613
Chris Hand <cj.hand@gmail.com>
2022-08-14 11:46:52 -0700

    My version of the assignment

--- GestureRecognizers/ViewController.swift
+++ GestureRecognizers/ViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var imageText: UILabel!
-    
+    private var index = 0
+    private var collection = [ImageCollection]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "wallpaper")!)
-        imageView.image = UIImage(named: "family")
-        imageText.text = "Family"
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePicture))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        index = 0
+        collection.append(ImageCollection(image: UIImage(named: "family")!, title: "Family"))
+        collection.append(ImageCollection(image: UIImage(named: "stella")!, title: "Stella"))
+        
+        imageView.image = collection[index].image
+        imageText.text = collection[index].title
+    }
+                                                       
+    @objc
+    private func changePicture() {
+        index += 1
+        if index >= collection.count {
+            index = 0
+        }
+        
+        imageView.image = collection[index].image
+        imageText.text = collection[index].title
+    }
+    
+    private struct ImageCollection {
+        var image: UIImage
+        var title: String
     }
 }
 
